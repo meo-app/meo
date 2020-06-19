62b32190b91c0e6c976803b1a7aee97b858a6995
Weslley Araujo <weslley.araujo@booking.com>
2020-06-19 21:37:52 +0200

    feat(App): Add react-native-safe-area-context

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
 import { useFormik } from "formik";
 import * as SQLite from "expo-sqlite";
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  useSafeArea,
+  initialWindowSafeAreaInsets,
+} from "react-native-safe-area-context";
 import React, { useEffect, useState } from "react";
 import {
   Alert,
@@ -23,7 +29,8 @@ function useForceUpdate(): [() => void, number] {
   return [() => setValue(value + 1), value];
 }
 
-export default function App() {
+function App() {
+  const insets = useSafeArea();
   const [items, setItems] = useState<any[]>([]);
   const [forceUpdate, forceUpdateId] = useForceUpdate();
   const formik = useFormik<Values>({
@@ -64,7 +71,6 @@ export default function App() {
   useEffect(() => {
     db.transaction((tx) => {
       tx.executeSql("select * from items", [], (_, { rows }) => {
-        console.log("setting items");
         setItems(
           [...Array(rows.length).keys()].map((index) => rows.item(index))
         );
@@ -73,32 +79,70 @@ export default function App() {
   }, [setItems, forceUpdateId]);
 
   return (
-    <KeyboardAvoidingView style={styles.container}>
-      <TextInput
-        placeholder="Write something"
-        value={formik.values.text}
-        onChangeText={(value) => formik.setFieldValue("text", value)}
-      />
-      <Button
-        title="Save"
-        onPress={() => formik.handleSubmit()}
-        disabled={!formik.isValid}
-      />
-      <Text>{JSON.stringify({ forceUpdateId })}</Text>
-      <View key={`items-${forceUpdateId}`}>
-        {items.map((item) => (
-          <Text key={item.id}>{item.value}</Text>
-        ))}
+    <SafeAreaView>
+      <View
+        style={{
+          padding: 24,
+          paddingTop: insets.top,
+          marginTop: -insets.top - 5,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 1,
+          },
+          shadowOpacity: 0.2,
+          shadowRadius: 1.41,
+          elevation: 2,
+          marginLeft: -1,
+          marginRight: -1,
+        }}
+      >
+        <Text
+          style={{
+            fontWeight: "bold",
+            fontSize: 36,
+          }}
+        >
+          Home
+        </Text>
       </View>
-    </KeyboardAvoidingView>
+      <KeyboardAvoidingView style={styles.container}>
+        <TextInput
+          placeholder="Write something"
+          value={formik.values.text}
+          onChangeText={(value) => formik.setFieldValue("text", value)}
+        />
+        <Button
+          title="Save"
+          onPress={() => formik.handleSubmit()}
+          disabled={!formik.isValid}
+        />
+        <Text>{JSON.stringify({ forceUpdateId })}</Text>
+        <View key={`items-${forceUpdateId}`}>
+          {items.map((item) => (
+            <Text key={item.id}>{item.value}</Text>
+          ))}
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
 });
+
+const Screen: React.FunctionComponent = function Screen() {
+  return (
+    <SafeAreaProvider>
+      <App />
+    </SafeAreaProvider>
+  );
+};
+
+export default Screen;
