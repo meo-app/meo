0489a5afecfd8c3c7f6d707aca13494b8861a73a
Weslley Araujo <weslleyaraujo20@gmail.com>
2020-07-13 09:59:59 +0200

    feat(react-navigation): playing with tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import {
   NavigationContainer,
   NavigationContainerRef,
+  Route,
+  useNavigation,
 } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "react-native-gesture-handler";
 import { Providers } from "./application/providers/Providers";
 import { Create } from "./application/screens/Create";
@@ -16,50 +19,55 @@ import { RouteNames } from "./route-names";
 import {
   TouchableHighlight,
   TouchableOpacity,
+  TapGestureHandler,
 } from "react-native-gesture-handler";
+import { View } from "react-native";
+import { Frame } from "./components/Frame";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const Placeholder = () => null;
+
+const AppTabs = () => {
+  return (
+    <Tab.Navigator
+      tabBar={({ navigation }) => (
+        <FloatingActions
+          onHomePress={() => navigation.navigate(RouteNames.Home)}
+          onCreatePress={() => {
+            navigation.emit<"tabPress">("tabPress");
+            navigation.navigate(RouteNames.Create);
+          }}
+          onSearchPress={() => navigation.navigate(RouteNames.Search)}
+        />
+      )}
+    >
+      <Tab.Screen name={RouteNames.Home} component={Home} />
+      <Tab.Screen
+        name={RouteNames.Create}
+        component={Placeholder}
+        listeners={({ navigation }) => ({
+          tabPress: () => {
+            console.warn("ontabpress create");
+          },
+        })}
+      />
+      <Tab.Screen name={RouteNames.Search} component={Search} />
+    </Tab.Navigator>
+  );
+};
 
 function Root() {
   const ref = useRef<NavigationContainerRef>(null);
   return (
-    <NavigationContainer ref={ref}>
-      <Stack.Navigator
-        mode="modal"
-        screenOptions={{
-          header: Header,
-        }}
-      >
-        <Stack.Screen name={RouteNames.Home} component={Home} />
-        <Stack.Screen
-          name={RouteNames.Create}
-          component={Create}
-          options={{
-            header: () => (
-              <SafeHeader>
-                <TouchableOpacity
-                  onPress={() => ref.current?.navigate(RouteNames.Home)}
-                >
-                  <Icon type="Close" size="small" />
-                </TouchableOpacity>
-              </SafeHeader>
-            ),
-          }}
-        />
-        <Stack.Screen name={RouteNames.Search} component={Search} />
-      </Stack.Navigator>
-      <FloatingActions
-        onHomePress={() => {
-          ref.current?.navigate(RouteNames.Home);
-        }}
-        onCreatePress={() => {
-          ref.current?.navigate(RouteNames.Create);
-        }}
-        onSearchPress={() => {
-          ref.current?.navigate(RouteNames.Search);
-        }}
-      />
-    </NavigationContainer>
+    <>
+      <NavigationContainer ref={ref}>
+        <Stack.Navigator mode="modal" headerMode="none">
+          <Stack.Screen name="App" component={AppTabs} />
+          <Stack.Screen name={RouteNames.Create} component={Create} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
