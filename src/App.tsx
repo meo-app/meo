45aff483cff8a7d53c2435fc9c9b92a6d467cab6
Weslley Araujo <weslleyaraujo20@gmail.com>
2020-07-28 13:19:46 +0200

    feat(*): format date

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import "intl";
+import "intl/locale-data/jsonp/en";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
