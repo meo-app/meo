c4e44f48dffc7d6aef2f83ed5662b0b76d873bbc
Weslley Araujo <weslleyaraujo20@gmail.com>
2021-03-14 09:33:49 +0100

    Empty state views

--- src/application/screens/Home.tsx
+++ src/application/screens/Home.tsx
@@ -1,5 +1,5 @@
 import { useScrollToTop } from "@react-navigation/native";
-import React, { useRef } from "react";
+import React, { useMemo, useRef } from "react";
 import { View } from "react-native";
 import { Font } from "../../components/Font";
 import { Header } from "../../components/Header";
@@ -7,19 +7,19 @@ import { PostsList } from "../../components/PostsList";
 import { QueryKeys } from "../../shared/QueryKeys";
 import { usePaginatedPosts } from "../../hooks/use-paginated-posts";
 import { useAppContext } from "../providers/AppProvider";
+import { Frame } from "../../components/Frame";
 
 function Home() {
-  const { data, error, isFetching, fetchNextPage } = usePaginatedPosts(
-    QueryKeys.POSTS,
-    {
-      queryFn: ({ limit, offset }) =>
-        `select * from posts order by id desc limit ${limit}, ${offset}`,
-    }
-  );
+  const { data, error, fetchNextPage } = usePaginatedPosts(QueryKeys.POSTS, {
+    queryFn: ({ limit, offset }) =>
+      `select * from posts order by id desc limit ${limit}, ${offset}`,
+  });
 
+  const isEmpty = useMemo(() => !data?.pages.flat(1).length, [data]);
   const { tabBarHeight } = useAppContext();
   const ref = useRef(null);
   useScrollToTop(ref);
+
   return (
     <View
       style={{
@@ -27,30 +27,18 @@ function Home() {
       }}
     >
       <Header title="Home" />
-      {isFetching && (
-        <View>
-          <Font>TODO: loading state</Font>
-        </View>
-      )}
       {error && (
-        <View
-          style={{
-            flex: 1,
-          }}
-        >
-          <Font variant="body">There was an error!</Font>
-        </View>
+        <Frame flex={1 / 2} justifyContent="center" alignItems="center">
+          <Font variant="body">There was an error, panic, panic</Font>
+        </Frame>
       )}
-      {data && !data?.pages?.length && (
-        <View
-          style={{
-            height: "100%",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Font variant="body">Thought of something? Add it here :)</Font>
-        </View>
+      {data && isEmpty && (
+        <Frame flex={1 / 2} justifyContent="center" alignItems="center">
+          <Font variant="subtitle">Thought of something?</Font>
+          <Frame marginTop="small">
+            <Font>Add it here :)</Font>
+          </Frame>
+        </Frame>
       )}
       <PostsList
         data={data}
