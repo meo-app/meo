5579315cfa1d064144ecb1c0e2c5e4764bd5d680
Weslley Araujo <weslleyaraujo20@gmail.com>
2021-05-29 21:38:10 +0200

    Backup: download file on Android

--- android/app/src/main/java/com/claudio/generated/BasePackageList.java
+++ android/app/src/main/java/com/claudio/generated/BasePackageList.java
@@ -16,6 +16,7 @@ public class BasePackageList {
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.lineargradient.LinearGradientPackage(),
         new expo.modules.location.LocationPackage(),
+        new expo.modules.medialibrary.MediaLibraryPackage(),
         new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.sqlite.SQLitePackage(),
         new expo.modules.updates.UpdatesPackage(),
--- src/screens/Settings/hooks/use-download-backup-handler.ts
+++ src/screens/Settings/hooks/use-download-backup-handler.ts
@@ -2,35 +2,10 @@ import * as FileSystem from "expo-file-system";
 import Share from "react-native-share";
 import { useMutation } from "react-query";
 import { useDB } from "../../../providers/SQLiteProvider";
-import * as Permissions from "expo-permissions";
 import * as MediaLibrary from "expo-media-library";
+import { Alert, Platform } from "react-native";
 
-// const perm = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
-// if (perm.status != "granted") {
-//   return;
-// }
-
-// try {
-//   const asset = await MediaLibrary.createAssetAsync(downloadedFile.uri);
-//   const album = await MediaLibrary.getAlbumAsync("Download");
-//   if (album == null) {
-//     await MediaLibrary.createAlbumAsync("Download", asset, false);
-//   } else {
-//     await MediaLibrary.addAssetsToAlbumAsync([asset], album, false);
-//   }
-// } catch (e) {
-//   handleError(e);
-// }
-
-/**
- * TODO:
- *
- * - [ ] support versioning on backup files
- * - [ ] Submission manager on download / upload etc
- * - [ ] Support Android
- */
-
-const FILE_URI = `${FileSystem.documentDirectory}/meo.backup`;
+const FILE_URI = `${FileSystem.documentDirectory}/meo.txt`;
 
 function useDownloadBackupHandler() {
   const db = useDB();
@@ -54,9 +29,31 @@ function useDownloadBackupHandler() {
       encoding: FileSystem.EncodingType.UTF8,
     });
 
-    Share.open({
-      url: `${FileSystem.documentDirectory}/meo.backup`,
-    });
+    if (Platform.OS === "ios") {
+      Share.open({ url: FILE_URI });
+      return;
+    } else if (Platform.OS === "android") {
+      const perm = await MediaLibrary.getPermissionsAsync();
+      if (perm.status !== "granted") {
+        const req = await MediaLibrary.requestPermissionsAsync();
+        if (req.status !== "granted") {
+          return;
+        }
+      }
+
+      try {
+        const asset = await MediaLibrary.createAssetAsync(FILE_URI);
+        const album = await MediaLibrary.getAlbumAsync("Download");
+        if (album === null) {
+          await MediaLibrary.createAlbumAsync("Download", asset, false);
+        } else {
+          await MediaLibrary.addAssetsToAlbumAsync([asset], album, false);
+        }
+        Alert.alert("Backup downloaded");
+      } catch (e) {
+        console.error(e);
+      }
+    }
   });
 }
 
